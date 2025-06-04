0a6c6cb75a9223899802f028788420246c25d2be
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-06-03 23:20:12 -0300

    Create FilterDictionary.cs

---
+++ structured/Service/Services/FilterService/FilterDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace Service.Services.FilterService;
+
+public class FilterDictionary
+{
+    private static readonly ConcurrentDictionary<string, DateTime> LastReadTimes = new();
+
+    private static readonly int FilterTimeInSeconds = 5;
+
+    public bool ShouldReportTag(string epc)
+    {
+        var now = DateTime.UtcNow;
+
+        if (LastReadTimes.TryGetValue(epc, out var lastReadTime))
+        {
+            if ((now - lastReadTime).TotalSeconds < FilterTimeInSeconds)
+            {
+                return false;
+            }
+        }
+
+        LastReadTimes[epc] = now;
+        return true;
+    }
+}
