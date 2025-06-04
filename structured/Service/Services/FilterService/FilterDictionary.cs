030a4bd50a27456ffc5e7dbaea934154ea1d759c
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-06-03 23:30:30 -0300

    Update FilterDictionary.cs

--- structured/Service/Services/FilterService/FilterDictionary.cs
+++ structured/Service/Services/FilterService/FilterDictionary.cs
@@ -6,7 +6,12 @@ public class FilterDictionary
 {
     private static readonly ConcurrentDictionary<string, DateTime> LastReadTimes = new();
 
-    private static readonly int FilterTimeInSeconds = 5;
+    private readonly int _filterTimeInSeconds;
+
+    public FilterDictionary(int filterTimeInSeconds)
+    {
+        _filterTimeInSeconds = filterTimeInSeconds;
+    }
 
     public bool ShouldReportTag(string epc)
     {
@@ -14,7 +19,7 @@ public class FilterDictionary
 
         if (LastReadTimes.TryGetValue(epc, out var lastReadTime))
         {
-            if ((now - lastReadTime).TotalSeconds < FilterTimeInSeconds)
+            if ((now - lastReadTime).TotalSeconds < _filterTimeInSeconds)
             {
                 return false;
             }
