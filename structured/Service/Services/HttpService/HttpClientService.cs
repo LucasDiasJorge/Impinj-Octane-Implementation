1bd437d6395828bf35622f5e20a7961a0db1f6ed
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-06-03 23:09:59 -0300

    Create HttpClientService.cs

---
+++ structured/Service/Services/HttpService/HttpClientService.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Text.Json;
+using Impinj.OctaneSdk;
+using Microsoft.Extensions.Configuration;
+
+namespace Service.Services.HttpService;
+
+public class HttpClientService
+{
+    
+    private readonly IConfiguration _config;
+
+    public HttpClientService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<bool> SendTagToApiAsync(Tag tag)
+    {
+        try
+        {
+            using HttpClient client = new();
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "<BEARER TOKEN>");
+
+            var tagData = new
+            {
+                Epc = tag.Epc.ToString(),
+                Antenna = tag.AntennaPortNumber,
+                Timestamp = tag.FirstSeenTime.Utc,
+                Rssi = tag.PeakRssiInDbm
+            };
+
+            Console.WriteLine(tagData);
+
+            var json = JsonSerializer.Serialize(tagData, new JsonSerializerOptions { WriteIndented = true });
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            Console.WriteLine($"Enviando dados para API: {json}");
+
+            var response = await client.PostAsync(_config["API:DestinationService"], content);
+            string responseBody = await response.Content.ReadAsStringAsync(); // Obtém o corpo da resposta
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"✔ Etiqueta {tag.Epc} reportada com sucesso!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"❌ Erro ao reportar {tag.Epc}");
+                Console.WriteLine($"Status Code: {response.StatusCode} ({(int)response.StatusCode})");
+                Console.WriteLine($"Response Headers: {response.Headers}");
+                Console.WriteLine($"Response Body: {responseBody}");
+            }
+        }
+        catch (HttpRequestException httpEx)
+        {
+            Console.WriteLine($"❗ Erro HTTP: {httpEx.Message}");
+        }
+        catch (TaskCanceledException tcEx)
+        {
+            Console.WriteLine($"⏳ Timeout ou requisição cancelada: {tcEx.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"⚠ Erro inesperado ao enviar dados para a API: {ex.Message}");
+        }
+        return false;
+
+    }
+    
+}
