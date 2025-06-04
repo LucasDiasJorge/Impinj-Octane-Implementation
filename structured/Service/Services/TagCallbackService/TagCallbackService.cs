6fccc2e6019ef7a4a01a3206a4d3b7283d41bbfb
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-06-03 23:20:14 -0300

    Create TagCallbackService.cs

---
+++ structured/Service/Services/TagCallbackService/TagCallbackService.cs
@@ -0,0 +1,26 @@
+﻿using Impinj.OctaneSdk;
+using Service.Services.FilterService;
+
+namespace Service.Services.TagCallbackService;
+
+public class TagCallbackService
+{
+    
+    private readonly FilterDictionary _filters;
+
+    public TagCallbackService(FilterDictionary filters)
+    {
+        _filters = filters;
+    }
+
+    public void OnTagsReported(object sender, TagReport report)
+    {
+        foreach (Tag tag in report)
+        {
+            if (_filters.ShouldReportTag(tag.Epc.ToString()))
+            {
+                // Enqueue
+            }
+        }
+    }
+}
